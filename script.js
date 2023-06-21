9b4a8f16587de59c0a98e83fca664c3385f65391
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-21 21:21:00 +0200

    added event listener for math symbols and created process function

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return Number(num1) / Number(num2); 
 };
+
+function changeSymbol() {
+    mathButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            symbol = button.innerHTML;
+            console.log(process(2, symbol, 4));
+        });
+    });
+};
+
+function process(firstDigit, mathSymbol, secondDigit) {
+    console.log(mathSymbol);
+    if (mathSymbol === "+") {
+        let result = add(firstDigit, secondDigit);
+        return result;
+    }
+    else if (mathSymbol === "-") {
+        let result = subtract(firstDigit, secondDigit);
+        return result;
+    }
+    else if (mathSymbol === "*") {
+        let result = multiply(firstDigit, secondDigit);
+        return result;
+    }
+    else {
+        let result = divide(firstDigit, secondDigit);
+        return result;
+    }
+};
+
+let firstNumber = 0
+let secondNumber = 0
+let symbol = ""
+
+let mathButtons = document.querySelectorAll(".basic-math-operations");
+let numbersButtons = document.querySelectorAll(".number");
+
+changeSymbol();
