8aea670adf1119258b3d1010fb21790c6d9d27b0
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-27 19:52:44 +0200

    create function and listener for buttons as well as function to perform calculations

--- script.js
+++ script.js
@@ -20,16 +20,35 @@ function divide(num1, num2) {
 
 // this function takes a button as a parameter and changes the math "symbol" to the selected symbol
 function changeSymbol(button) {
-    symbol = button.innerHTML;
-    return symbol;
+    operator = button.innerHTML;
+    return operator;
 };
 
-// this function checks whether a symbol has been selected from the math operators
-function checkSymbol() {
-    if (symbol === "") {
-        return false;
+// thish function takes a button and an operator as parameters and depending the condition 
+// of the operator updates either the first or second number
+function getNumbers(operator, button) {
+    if (operator === "") {
+        firstNumber += button.innerHTML;
+        console.log(firstNumber);
     } else {
-        return true;
+        secondNumber += button.innerHTML;
+        console.log(secondNumber);
+    };
+};
+
+// this functions takes 2 numbers and a math operator which it performs calculations on
+function process(firstDigit, operator, secondDigit) {
+    if (operator === "+") {
+        return add(firstDigit, secondDigit);
+    }
+    else if (operator === "-") {
+        return subtract(firstDigit, secondDigit);
+    }
+    else if (operator === "*") {
+        return multiply(firstDigit, secondDigit);
+    }
+    else if (operator === "/") {
+        return divide(firstDigit, secondDigit);
     };
 };
 
@@ -37,19 +56,42 @@ let numbers = document.querySelectorAll(".number");
 let mathOperators = document.querySelectorAll(".basic-math-operations");
 let equalSign = document.getElementById("equal-sign");
 
-let symbol = "";
+let operator = "";
 let symbolIsChanged = false;
+
+let firstNumber = "";
+let secondNumber = "";
 
 // this event listener listens for a click event on one of the math operators buttons 
 // and updates the symbol varibale accordingly
 mathOperators.forEach((button) => {
     button.addEventListener("click", () => {
-        checkSymbol();
-        symbol = changeSymbol(button);
-        checkSymbol();
+        if (operator === "") {
+            operator = changeSymbol(button);
+            console.log(operator);
+        } else {
+            firstNumber = process(firstNumber, operator, secondNumber);
+            operator = changeSymbol(button);
+            secondNumber = "";
+            console.log(firstNumber);
+            console.log(operator);
+        };
     });
 });
 
+// this event listener listens for a click event and depending on whether 
+// the symbol has beel selected, gets the numbers to be calculated
+numbers.forEach((button) => {
+    button.addEventListener("click", () => {
+        getNumbers(operator, button);
+    });
+});
+
+// this listener listens for a "click" event on the equal sign and processes the 
+// calculations while resetting the first and second number for a fresh restart
 equalSign.addEventListener("click", () => {
-    
+    console.log(process(firstNumber, operator, secondNumber));
+    operator = "";
+    firstNumber = "";
+    secondNumber = "";
 });
